f13fc93d84e3f050765f5aca1d5900f58478c84d
miurellcristina <miurell.castillo@uabc.edu.mx>
2021-12-02 00:27:26 -0800

    actualizacion de inspeccion

--- PrototipoC/contenido/inspeccion.php
+++ PrototipoC/contenido/inspeccion.php
@@ -1,3 +1,4 @@
+ 
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -11,9 +12,61 @@
                 <link rel="stylesheet" href="../recursos/css/menu.css">
                 <link rel="stylesheet" href="../recursos/css/font.css">
                 <link rel="icon" href="../recursos/img/favicon.ico">
+                
     </head>
-    <body>
+    <body >
         <?php include '../recursos/php/encabezadologo.php';?>
+
+        <section class="sect" id="inspecion">
+            <div class="Tittle">
+                <h1>¿En que consiste el servicio de Inspección oficial?</h1>
+            </div>
+            <div class="parrafo">
+                <p>
+                    Se basa en la <b>Norma Oficial Mexicana (NOM-010-SCFI- 1994)</b> que
+                    describe las pruebas que se deben realizar como son:
+                    <br> 
+                <p class="i">
+                    <i class="fas fa-check-circle i">Inspección ocular</i><br>
+                    <i class="fas fa-check-circle i">Prueba de excentricidad</i><br> 
+                    <i class="fas fa-check-circle i">Prueba de repetibilidad</i><br>
+                    <i class="fas fa-check-circle i">Prueba de exactitud</i><br>
+                    <i class="fas fa-check-circle i">Dictaminar</i><br>
+                </p>
+                <p>
+                    Si los resultados demuestran que el instrumento de medición funciona
+                    adecuadamente y con la exactitud que establece la Norma Oficial Mexicana aplicable, la
+                    unidad de inspección deberá colocar un holograma de “Instrumento Inspeccionado” y
+                    adherirle un sellos o etiqueta distintivo de la Unidad. 
+                </p>
+            </div><br><br><br>
+            
+        </section>
+        <h1 class="Tittle">La inspección se divide en dos tipos:</h1>
+        <section id="tipos">
+            <div class="con-p">
+                <div class="const"><img src="../recursos/IMG/IP.png" alt=""></div>
+                <div class="const"><img src="../recursos/IMG/IE.png" alt=""></div>
+            </div>
+        </section>
+            <div class="Tittle">
+                <h3>Periodos de Inspección</h3>
+                <h3 class="sub">Una vez cada año</h3>
+            </div>
+        <section>
+            <div class="con-p">
+                <div class="const"><img src="../recursos/IMG/1sp.png" alt=""></div>
+                <div class="const"><img src="../recursos/IMG/1se.png" alt=""></div>>
+            </div><br><br>
+            <h3  class="sub">Dos veces al año</h3>
+            <div class="con-p">
+                <div clas="const-img"><img src="../recursos/IMG/2s1.png" alt=""></div>
+                <div clas="const-img"><img src="../recursos/IMG/2s4.png" alt=""></div>
+                <div clas="const-img"><img src="../recursos/IMG/2s2.png" alt=""></div>
+                <div clas="const-img"><img src="../recursos/IMG/2s3.png" alt=""></div>
+            </div>
+        </section>
     </body>
+    <script src="https://kit.fontawesome.com/20529a5ad9.js" crossorigin="anonymous"></script>
 
 </html> 
